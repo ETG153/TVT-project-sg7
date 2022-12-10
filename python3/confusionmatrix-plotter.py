Plot NN confusion matrix and fix plotter incorrect value bug

python3/confusionmatrix-plotter.py
@@ -2,17 +2,17 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-inputfilepath = "arduino/Kmeans-test-results.csv"
-#inputfilepath = "arduino/NN-test-results.csv"
-outputfilepath = "documentation/kmeans-confusionmatrix-results.png"
-#outputfilepath = "documentation/nn-confusionmatrix-results.png"
+#inputfilepath = "arduino/Kmeans-test-results.csv"
+inputfilepath = "arduino/NN-test-results.csv"
+#outputfilepath = "documentation/kmeans-confusionmatrix-results.png"
+outputfilepath = "documentation/nn-confusionmatrix-results.png"
 
 results = pd.read_csv(inputfilepath)
 #print(results)
 
 confusionmatrix = pd.crosstab(results["Estimate"], results["Answer"], rownames=["Actual"], colnames=["Estimate"])
-matrixrowsums = confusionmatrix.sum()
-normalizedmatrix = confusionmatrix.divide(matrixrowsums)
+matrixrowsums = confusionmatrix.sum(axis="columns")
+normalizedmatrix = confusionmatrix.divide(matrixrowsums, axis="index")
 #print(confusionmatrix)
 #print(matrixrowsums)
 #print(normalizedmatrix)